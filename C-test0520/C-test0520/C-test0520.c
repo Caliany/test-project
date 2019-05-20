eeb5e5f98719814e5d02c48ecb97d4695595ecde
Caliany <2275928214@qq.com>
2019-05-20 22:28:25 +0800

    运动员跳水问题

--- C-test0520/C-test0520/C-test0520.c
+++ C-test0520/C-test0520/C-test0520.c
@@ -1,218 +1,116 @@
-//1.递归和非递归分别实现求第n个斐波那契数。 
+#if 0
+1.
+5位运动员参加了10米台跳水比赛，有人让他们预测比赛结果
+A选手说：B第二，我第三；
+B选手说：我第二，E第四；
+C选手说：我第一，D第二；
+D选手说：C最后，我第三；
+E选手说：我第四，A第一；
+比赛结束后，每位选手都说对了一半，请编程确定比赛的名次
+#endif
 #include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-
-int main()     //非递归实现
-{
-	int n = 0;
-	int a = 1;
-	int b = 1;
-	int c = a;
-	printf("请输入要求的第n个斐波那契数n=");
-	scanf("%d", &n);
-	while (n>2)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	printf("%d\n", c);
-	system("pause");
-	return 0;
-}
-//
-//for (i = 2; i <= n; i++)
-//{
-//	an = an_1 + an_2;
-//	an_2 = an_1;
-//	an_1 = an;
-//}
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-
-int fib(int n)
-{
-	if (n <= 2)
-		return 1; //1 1 2 3 5 8 13 21 34 55.... 前两个数为1，所以小于等于2时返回为1
-	else
-		return fib(n - 1) + fib(n - 2); //递归实现(数1的过程，效率低，写循环吧！)；循环层数不确定时，写递归
-}
-int main()
-{
-	int n = 0;
-	printf("请输入要求的第n个斐波那契数n=");
-	scanf("%d", &n);
-	int ret = fib(n);
-	printf("%d\n", ret);
-	system("pause");
-	return  0;
-}
-
-//2.编写一个函数实现n^k，使用递归实现 
-#include<stdio.h>
-#include<stdlib.h>
-int del(int n, int k, int ant)
-{
-	if (n != 1)
-	{
-		return del(n - 1, k, ant*k);
-	}
-	else
-		return ant*k;
-}
-
-int main()
-{
-	int num = 0;
-	int n = 0;
-	int k = 0;
-	int tmp = 1;
-	printf("n和k的值分别为：");
-	scanf_s("%d%d", &n, &k);
-	num = del(k, n, tmp);
-	printf("%d", num);
-	system("pause");
-	return 0;
-}
-
-//3. 写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和，
-#include<stdio.h>
-#include<stdlib.h>
-int DigitSum(int n)
-{
-	if (n > 0)
-	{
-		return (n % 10) + DigitSum(n / 10);
-	}
-	return 0;
-}
-int main()
-{
-	int n = 0;
-	printf("请输入一个数字：");
-	scanf("%d", &n);
-	printf("%d\n", DigitSum(n));
-	system("pause");
-	return 0;
-}
-//4. 编写一个函数 reverse_string(char * string)（递归实现）
-//实现：将参数字符串中的字符反向排列。
-//要求：不能使用C函数库中的字符串操作函数。
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-
-char *reverse_string(char *str)
-{
-	assert(str != NULL);
-	if (*str != 0)
-	{
-		str++;
-		reverse_string(str);
-		printf("%c ", *(str - 1));
-
-	}
-	return str;
-}
-
-int main()
-{
-	char arr[] = "abcdefd";
-	char *ret = reverse_string(arr);
-	system("pause");
-	return 0;
-}
-
-
-//5.递归和非递归分别实现strlen
-#include<stdio.h>
-#include<stdlib.h>
-
-int my_strlen(const char *str)//递归法实现
-{
-	if (0 == *str)
-		return 0;
-	else
-		return 1 + my_strlen(str + 1);
-
-}
-int main()
-{
-	const char *p = "abcdefd";
-	int len = my_strlen(p);
-	printf("%d\n", len);
-	system("pause");
-	return 0;
-}
-
-//6.递归和非递归分别实现求n的阶乘 
-#include<stdio.h>
-#include<stdlib.h>
-
-int fac(int n)//非递归法实现
-{
-	int i = 0;
-	int tmp = 1;
-	for (i = 1; i <= n; i++)
-	{
-		tmp *= i;
-	}
-	return tmp;
-}
-int main()
-{
-	int n = 0;
-	printf("请输入一个数字：");
-	scanf("%d", &n);
-	int fac(n);
-	printf("%d\n", fac(n));
-	system("pause");
-	return 0;
-}
-
-int fac(int n)  //递归法实现
-{
-	if (n <= 1)
-	{
-		return 1;
-	}
-	else
-	{
-		return n*fac(n - 1);
-	}
-}
-int main()
-{
-	int n = 0;
-	printf("请输入一个数字：");
-	scanf("%d", &n);
-	int fac(n);
-	printf("%d\n", fac(n));
-	system("pause");
-	return 0;
-}
-
-//7.递归方式实现打印一个整数的每一位
-#include<stdio.h>
-#include<stdlib.h>
 #include<windows.h>
 
-void print(int n)
+int main0()
 {
-	if (n >9)
+	int a, b, c, d, e;
+	for (a = 1; a <= 5; a++)
 	{
-		print(n / 10);
+		for (b = 1; b <= 5; b++)
+		{
+			for (c = 1; c <= 5; c++)
+			{
+				for (d = 1; d < 5; d++)
+				{
+					for (e = 1; e < 5; e++)
+					{
+						if (a*b*c*d*e == 120)
+						{
+							if (((b == 2) + (a == 3) == 1) &&
+								((b == 2) + (e == 4) == 1) &&
+								((c == 1) + (d == 2) == 1) &&
+								((c == 5) + (d == 3) == 1) &&
+								((e == 4) + (a == 1) == 1))
+							{
+								printf("a=%d,b=%d,c=%d,d=%d,e=%d\n", a, b, c, d, e);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	printf("%d ", n % 10);
+	system("pause");
+	return 0;
 }
+#if 0
+2.
+日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为4个
+嫌疑犯的一个。以下为4个嫌疑犯的供词。
+A说：不是我。
+B说：是C。
+C说：是D。
+D说：C在胡说
+已知3个人说了真话，1个人说的是假话。
+现在请根据这些信息，写一个程序来确定到底谁是凶手。
+#endif
+#include<stdio.h>
+#include<stdlib.h>
+
+int main1()
+{
+	int killer = 0;
+	for (killer = 'A'; killer <= 'D'; killer++)
+	{
+		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
+		{
+			printf("killer=%c\n", killer);
+			system("pause");
+			return 0;
+		}
+	}
+	return 1;
+}
+#if 0
+3.在屏幕上打印杨辉三角。
+1
+1 1
+1 2 1
+1 3 3 1
+#endif
+#include <stdio.h>
+#include<stdlib.h>
+
+#define N 10
+
 int main()
 {
-	int num;
-	scanf("%d", &num);
-	print(num);
+	int arr[N][N] = { 0 };
+	int i = 0;
+	for (i = 0; i<N; i++)
+	{
+		int m = 0;
+		for (m = 0; m<N - i; m++)
+		{
+			printf("  ");
+		}
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			if ((0 == j) || (i == j))
+			{
+				arr[i][j] = 1;
+
+			}
+			else
+			{
+				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+
+			}
+			printf("%4d", arr[i][j]);
+		}
+		printf("\n");
+	}
 	system("pause");
 	return 0;
 }
